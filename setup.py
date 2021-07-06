e35859a4fce98fc8a78c5fab8ac1c6ca1d752dad
John Oram <john@oram.ca>
2021-07-05 21:54:30 -0700

    moar data

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mec_items",
+    name="trails",
     version="0.0.1",
     author="John Oram",
     author_email="john@oram.ca",
-    description="A small example package",
+    description="A small package of hiking and mountaineering trails",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joram/mec_items",
+    url="https://github.com/joram/trails",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
